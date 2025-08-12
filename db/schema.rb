93cfc2d6c18f248d569d34093f800b6119353168
Haris_Ali <siddiquiharis92@gmail.com>
2025-08-12 11:12:32 +0530

    Updated portfolio to include attribute for position

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_10_212941) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_12_051757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,6 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_10_212941) do
     t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
   end
 
   create_table "skills", force: :cascade do |t|
