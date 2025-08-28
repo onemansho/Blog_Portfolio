b70bc8f9211a932850d7e8836e61038f33484bce
Haris_Ali <siddiquiharis92@gmail.com>
2025-08-28 18:06:32 +0530

    Updated file upload forms and refactored placeholder functionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,17 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+    "https://placehold.co/#{height}x#{width}"
+  end 
+
+  def portfolio_img(img, type)
+    if img.present? && img.url.present?
+      img.url
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200')
+    elsif type == 'main'
+      image_generator(height: '600', width: '400')
+    end
+ end
+
 end
+
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,28 +2,23 @@ class Portfolio < ApplicationRecord
   has_many :technologies, dependent: :destroy
   accepts_nested_attributes_for :technologies, reject_if:
    lambda{|attrs| attrs['name'].blank?}
-  include Placeholder
- validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
+  
+  validates_presence_of :title, :subtitle, :body
 
- mount_uploader :thumb_image, PortfolioUploader
- mount_uploader :main_image, PortfolioUploader
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 
- def self.angular
-  where(subtitle:"Angular")
- end
+  def self.angular
+    where(subtitle:"Angular")
+  end
 
- def self.by_position
-  order("position ASC")
- end
+  def self.by_position
+   order("position ASC")
+  end
  
- scope :ruby_on_rails_protfolio_items, ->{where(subtitle: 'Ruby on Rails')}
+  scope :ruby_on_rails_protfolio_items, ->{where(subtitle: 'Ruby on Rails')}
  
- after_initialize :set_default
 
- def set_default
-  self.main_image ||=Placeholder.image_generator(height: '400',width: '150')
-  self.thumb_image ||=Placeholder.image_generator(height: '400',width:'150' )
-
- end
+  
  
 end
